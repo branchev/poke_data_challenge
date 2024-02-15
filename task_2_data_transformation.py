bd3b861e5cdd38ed62b94926bb70985374e31793
branchev <ranchev.boris@abv.bg>
2024-02-15 20:46:22 +0200

    Refactor error and exception handling

--- task_2_data_transformation.py
+++ task_2_data_transformation.py
@@ -30,7 +30,6 @@ def calculate_bmi(pokemon):
     bmi = pokemon['weight'] / (pokemon['height'] ** 2)
     bmi = round(bmi, 2)
     pokemon['bmi'] = bmi
-
     return pokemon
 
 
@@ -41,22 +40,16 @@ def update_json_file(pokemon_data):
         pokemon_data (dict): A dictionary containing the modified Pokemon data.
     """
     pokemon_id, updated_data = pokemon_data
-
     try:
-        with open('pokemon_details.json', 'r') as file:
+        with open('pokemon_details.json', 'r+') as file:
             data = json.load(file)
-    except FileNotFoundError:
-        data = {}
-
-    data[pokemon_id] = updated_data
-
-    try:
-        with open('pokemon_details.json', 'w') as file:
+            data[pokemon_id] = updated_data
+            file.seek(0)
             json.dump(data, file, indent=4)
-        return data
+    except FileNotFoundError:
+        print("Error: JSON file 'pokemon_details.json' not found.")
     except Exception as e:
         print(f"An error occurred while updating the JSON file: {e}")
-        return None
 
 
 def run_pipeline():
@@ -76,40 +69,54 @@ def run_pipeline():
 
     Raises:
         FileNotFoundError: If the 'pokemon_details.json' file is not found.
+        ValueError: If the 'bmi' key already exists in the Pokemon data.
     
     Note 2:
         Execution of the task_1_data_extraction.py will provide a JSON file with desired content.
         This will allow the execution of this script.
     """
+    try:
+        with open('pokemon_details.json', 'r') as file:
+            pokemon_data = json.load(file)
+            # Checking if 'bmi' key exists - the data is already transformed once
+            if 'bmi' in pokemon_data[next(iter(pokemon_data))]:
+                raise ValueError("BMI already calculated and data is updated. Pipeline cannot proceed.")
 
-    with open('pokemon_details.json', 'r') as file:
-        pokemon_data = json.load(file)
-        # Checking if 'bmi' key exists - the data is already transformed once
-        if 'bmi' in pokemon_data[next(iter(pokemon_data))]:
-            raise ValueError("BMI already calculated and data is updated. Pipeline cannot proceed.")
+        with beam.Pipeline() as pipeline:
+            transformed_data = (
+                pipeline
+                | 'Read JSON File' >> beam.Create(pokemon_data.items())
+                | 'Normalize Pokemon Data' >> beam.Map(lambda kv: (kv[0], normalize_pokemon(kv[1])))
+                | 'Calculate BMI' >> beam.Map(lambda kv: (kv[0], calculate_bmi(kv[1])))
+                | 'Update JSON' >> beam.Map(update_json_file)
+            )
 
-    with beam.Pipeline() as pipeline:
-        transformed_data = (
-            pipeline
-            | 'Read JSON File' >> beam.Create(pokemon_data.items()) 
-            | 'Normalize Pokemon Data' >> beam.Map(lambda kv: (kv[0], normalize_pokemon(kv[1])))
-            | 'Calculate BMI' >> beam.Map(lambda kv: (kv[0], calculate_bmi(kv[1])))
-            | 'Update JSON' >> beam.Map(update_json_file)
-        )
+    except FileNotFoundError:
+        print("Error: JSON file 'pokemon_details.json' not found.")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
 def print_pokemon_details():
     """
     Prints the Pokemon details
     """
-    with open('pokemon_details.json', 'r') as file:
-        pokemon_data = json.load(file)
-    
-    for index, (pokemon_id, details) in enumerate(pokemon_data.items(), start=1):
-        print(f"{index}: {details['name'].capitalize()} - "
-              f"ID: {pokemon_id}, "
-              f"Height: {details['height']:.2f} m., "
-              f"Weight: {details['weight']:.2f} kg., "
-              f"BMI: {details['bmi']:.2f}")
+    try:
+        with open('pokemon_details.json', 'r') as file:
+            pokemon_data = json.load(file)
+
+        for index, (pokemon_id, details) in enumerate(pokemon_data.items(), start=1):
+            print(f"{index}: {details['name'].capitalize()} - "
+                  f"ID: {pokemon_id}, "
+                  f"Height: {details['height']:.2f} m., "
+                  f"Weight: {details['weight']:.2f} kg., "
+                  f"BMI: {details['bmi']:.2f}")
+    except FileNotFoundError:
+        print("Error: JSON file 'pokemon_details.json' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 if __name__ == '__main__':
